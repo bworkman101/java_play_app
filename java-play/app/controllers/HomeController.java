b10bdcdfa6803d61f013f3a80b92a2cc880aab3d
Benjamin Workman <benjamin.workman@gmail.com>
2016-08-11 10:29:58 -0600

    change title

--- java-play/app/controllers/HomeController.java
+++ java-play/app/controllers/HomeController.java
@@ -17,7 +17,7 @@ public class HomeController extends Controller {
    * <code>GET</code> request with a path of <code>/</code>.
    */
   public Result index() {
-      return ok(index.render("Let's Begin"));
+      return ok(index.render("Java Play Framework"));
     }
 
 }
